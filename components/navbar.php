137fd8db0e90b118302f69673642d39422a97a6a
reva007kali <le.revaldy@gmail.com>
2025-01-05 09:45:42 +0700

    navbar update

--- components/navbar.php
+++ components/navbar.php
@@ -5,11 +5,11 @@
         <a href="/"><img class="navbar-logo logo-l logo-round" src="img/logo/logo2.jpeg" alt=""></a>
     </div>
     <div class="navbar-menu">
-        <a class="navbar-link" href="home.php">Home</a>
-        <a class="navbar-link" href="about.php">About us</a>
-        <a class="navbar-link" href="jobs.php">Job Portal</a>
-        <a class="navbar-link" href="crewing-services.php">Crewing Services</a>
-        <a class="navbar-link" href="contact.php">Contact us</a>
+        <a class="navbar-link" href="/">Home</a>
+        <a class="navbar-link" href="/about">About us</a>
+        <a class="navbar-link" href="/jobs">Job Portal</a>
+        <a class="navbar-link" href="/crewing-services">Crewing Services</a>
+        <a class="navbar-link" href="/contact">Contact us</a>
     </div>
     <div class="navbar-extra">
         <?php include 'dm_button.php' ?>
